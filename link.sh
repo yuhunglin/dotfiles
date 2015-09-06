af62be510ee6d872063308262b65d60fe0707e11
Yu-Hung Lin <yuhung@yuhunglin.com>
2015-09-05 21:00:42 -0700

    git osxkeychain credentials

--- link.sh
+++ link.sh
@@ -9,3 +9,6 @@ ln -si ~/workspace/dotfiles/.gitconfig ~/.gitconfig
 cp -i ~/workspace/dotfiles/.bashrc ~/.bashrc
 cp -i ~/workspace/dotfiles/.bash_profile ~/.bash_profile
 cp -i ~/workspace/dotfiles/.inputrc ~/.inputrc
+
+mkdir -p ~/.aws/
+yes n | cp -i aws-credentials-sample ~/.aws/credentials 2>/dev/null
